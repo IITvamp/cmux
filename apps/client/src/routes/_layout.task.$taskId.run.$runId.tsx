b04c5228fd35efd73fdcb0465af1391e6d9b0620
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-07-09 09:56:39 -0700

    wip

--- apps/client/src/routes/_layout.task.$taskId.run.$runId.tsx
+++ apps/client/src/routes/_layout.task.$taskId.run.$runId.tsx
@@ -12,68 +12,15 @@ export const Route = createFileRoute("/_layout/task/$taskId/run/$runId")({
 
 function TaskRunComponent() {
   const { runId } = Route.useParams();
-  // Subscribe so that log updates stream in real-time.
   const taskRun = useQuery(api.taskRuns.subscribe, {
     id: runId as Id<"taskRuns">,
   });
 
   const { terminals } = useTerminals();
   const terminal = terminals.get(runId);
-  console.log("runid", runId);
-  console.log("terminals", terminals);
-  console.log("terminal", terminal);
-
-  // const { terminal, isReady } = useTerminal({
-  //   id: runId,
-  //   name: `Task Run ${runId}`,
-  //   autoCreate: false,
-  // });
-
-  // const terminalRef = useRef<HTMLDivElement>(null);
-  // const isAttachedRef = useRef(false);
-  // const lastLogLengthRef = useRef(0);
-
-  // Attach terminal to DOM
-  // useEffect(() => {
-  //   if (!terminal || !terminalRef.current || isAttachedRef.current) return;
-
-  //   terminal.xterm.open(terminalRef.current);
-  //   isAttachedRef.current = true;
-  //   terminal.elementRef = terminalRef.current;
-  //   terminal.fitAddon.fit();
-
-  //   const handleResize = () => {
-  //     if (terminal.fitAddon) {
-  //       terminal.fitAddon.fit();
-  //     }
-  //   };
-
-  //   const resizeObserver = new ResizeObserver(handleResize);
-  //   resizeObserver.observe(terminalRef.current);
-
-  //   return () => {
-  //     resizeObserver.disconnect();
-  //   };
-  // }, [terminal, isReady]);
-
-  // Write log content to terminal
-  // useEffect(() => {
-  //   if (!terminal || !taskRun?.log) return;
-
-  //   // Only write new content
-  //   if (taskRun.log.length > lastLogLengthRef.current) {
-  //     const newContent = taskRun.log.slice(lastLogLengthRef.current);
-  //     terminal.xterm.write(newContent);
-  //     lastLogLengthRef.current = taskRun.log.length;
-  //   }
-  // }, [terminal, taskRun?.log]);
-
-  if (!taskRun) {
-    return <div className="p-8">Loading...</div>;
-  }
 
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-row h-full">
       <div className="flex-1 overflow-auto">
         <div className="flex flex-col h-full space-y-6">
           <div className="flex flex-col flex-1 grow">
@@ -89,6 +36,7 @@ function TaskRunComponent() {
           </div>
         </div>
       </div>
+      <div>git diff</div>
     </div>
   );
 }
