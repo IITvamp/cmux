e89924df014e9c8c99140915cf7652e33529ce98
austinpower1258 <austinwang115@gmail.com>
2025-08-10 22:12:34 -0700

    add stuff

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,17 +1,17 @@
 import { createFileRoute, Navigate } from '@tanstack/react-router'
-// import { useUser } from '@stackframe/react'
+import { useUser } from '@stackframe/react'
 
 export const Route = createFileRoute('/')({
   component: IndexRoute,
 })
 
 function IndexRoute() {
-  // const user = useUser({ or: 'return-null' })
+  const user = useUser({ or: 'return-null' })
   
   // If user is logged in, redirect to dashboard
-  // if (user) {
-  //   return <Navigate to="/dashboard" />
-  // }
+  if (user) {
+    return <Navigate to="/dashboard" />
+  }
   
   // Otherwise, show the landing page
   return <Navigate to="/landing" />
