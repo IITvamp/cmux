8248a323707b7d5c3e8b0a0e6aeda00aef6821c9
austinpower1258 <austinwang115@gmail.com>
2025-08-29 18:06:35 -0700

    move stacktheme to layout

--- apps/www/app/handler/[...stack]/page.tsx
+++ apps/www/app/handler/[...stack]/page.tsx
@@ -1,10 +1,8 @@
-import { StackHandler, StackTheme } from "@stackframe/stack";
+import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "@/lib/utils/stack";
 
 export default function Handler(props: unknown) {
   return (
-    <StackTheme>
       <StackHandler fullPage app={stackServerApp} routeProps={props} />
-    </StackTheme>
   );
 }
--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { StackProvider } from "@stackframe/stack";
+import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackServerApp } from "../lib/utils/stack";
 
 const geistSans = Geist({
@@ -25,9 +25,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StackProvider app={stackServerApp}>
-          {children}
-        </StackProvider>
+        <StackTheme>
+          <StackProvider app={stackServerApp}>
+            {children}
+          </StackProvider>
+        </StackTheme>
       </body>
     </html>
   );
