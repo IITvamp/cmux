acaa99c7666d8fb2146c5844ffd0886b926c87e7
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-09-01 18:28:12 -0700

    wip

--- apps/client/src/components/ui/dropdown.tsx
+++ apps/client/src/components/ui/dropdown.tsx
@@ -1,8 +1,6 @@
 import type { DropdownExports } from "./dropdown.parts";
 import { DropdownParts } from "./dropdown.parts";
 
-// Aggregate object export only, to keep this file free of component exports
 export const Dropdown: DropdownExports = {
   ...DropdownParts,
 };
-
