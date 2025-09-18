33d53070a4eaf26d6f31e19303d1ec9edb39cbb8
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-09-18 11:25:32 -0700

    update default morph

--- apps/www/lib/utils/morph-defaults.ts
+++ apps/www/lib/utils/morph-defaults.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_nnucpxen";
-
+// export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_nnucpxen";
+export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_osr8ngj4";
