a65e46c5ce88b27cfa7ce2adf2f16a07b01decf7
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-09-22 11:29:37 -0700

    wip

--- apps/client/src/lib/monaco-environment.ts
+++ apps/client/src/lib/monaco-environment.ts
@@ -1,40 +1,33 @@
-import type { Environment } from "monaco-editor/esm/vs/editor/editor.api";
+import loader from "@monaco-editor/loader";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
-import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import MarkdownWorker from "monaco-editor/esm/vs/language/markdown/markdown.worker?worker";
-import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import * as monaco from "monaco-editor";
 
-const workerFactories: Record<string, () => Worker> = {
-  css: () => new CssWorker(),
-  javascript: () => new TsWorker(),
-  json: () => new JsonWorker(),
-  markdown: () => new MarkdownWorker(),
-  scss: () => new CssWorker(),
-  typescript: () => new TsWorker(),
-  html: () => new HtmlWorker(),
-  handlebars: () => new HtmlWorker(),
-};
-
-const defaultWorkerFactory = () => new EditorWorker();
-
-const environment: Environment = {
-  getWorker(_moduleId, label) {
-    const workerFactory = workerFactories[label] ?? defaultWorkerFactory;
-    return workerFactory();
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === "json") {
+      return new jsonWorker();
+    }
+    if (label === "css" || label === "scss" || label === "less") {
+      return new cssWorker();
+    }
+    if (label === "html" || label === "handlebars" || label === "razor") {
+      return new htmlWorker();
+    }
+    if (label === "typescript" || label === "javascript") {
+      return new tsWorker();
+    }
+    return new editorWorker();
   },
 };
 
-declare global {
-  // eslint-disable-next-line no-var
-  var MonacoEnvironment: Environment | undefined;
-}
-
-if (typeof globalThis !== "undefined") {
-  const shouldInstall = typeof globalThis.MonacoEnvironment === "undefined";
-  if (shouldInstall) {
-    globalThis.MonacoEnvironment = environment;
-  }
-}
+loader.config({
+  monaco,
+});
+export const loaderInitPromise = new Promise<typeof monaco>((resolve) => {
+  loader.init().then(resolve);
+});
