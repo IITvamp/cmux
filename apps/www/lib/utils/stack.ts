41578d995da2f07d7ad0f3bcbfde965ac0fa01b1
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-09-23 14:25:45 -0700

    wip

--- apps/www/lib/utils/stack.ts
+++ apps/www/lib/utils/stack.ts
@@ -1,26 +1,8 @@
 import { env } from "@/lib/utils/www-env";
-import * as StackframeJsPkg from "@stackframe/js";
-import * as StackframeStackPkg from "@stackframe/stack";
+import { StackServerApp as StackServerAppJs } from "@stackframe/js";
+import { StackServerApp } from "@stackframe/stack";
 
-type StackServerAppJsType = import("@stackframe/js").StackServerApp<true, string>;
-type StackServerAppType = import("@stackframe/stack").StackServerApp<true, string>;
-
-type StackServerAppJsCtor = new (...args: ConstructorParameters<typeof import("@stackframe/js").StackServerApp>) => import("@stackframe/js").StackServerApp;
-type StackServerAppCtorType = new (...args: ConstructorParameters<typeof import("@stackframe/stack").StackServerApp>) => import("@stackframe/stack").StackServerApp;
-
-const resolveExport = (mod: unknown, key: string) => {
-  const moduleObj = mod as { [k: string]: unknown; default?: Record<string, unknown> };
-  return (moduleObj && moduleObj[key]) ?? moduleObj?.default?.[key];
-};
-
-const StackServerAppJsCtorResolved = resolveExport(StackframeJsPkg, "StackServerApp") as StackServerAppJsCtor | undefined;
-const StackServerAppCtorResolved = resolveExport(StackframeStackPkg, "StackServerApp") as StackServerAppCtorType | undefined;
-
-if (!StackServerAppJsCtorResolved || !StackServerAppCtorResolved) {
-  throw new Error("StackServerApp export not found in stackframe packages");
-}
-
-export const stackServerApp = new StackServerAppCtorResolved({
+export const stackServerApp = new StackServerApp({
   projectId: env.NEXT_PUBLIC_STACK_PROJECT_ID,
   publishableClientKey: env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
   secretServerKey: env.STACK_SECRET_SERVER_KEY,
@@ -29,11 +11,11 @@ export const stackServerApp = new StackServerAppCtorResolved({
     afterSignIn: "/handler/after-sign-in",
     afterSignUp: "/handler/after-sign-in",
   },
-}) as StackServerAppType;
+});
 
-export const stackServerAppJs = new StackServerAppJsCtorResolved({
+export const stackServerAppJs = new StackServerAppJs({
   projectId: env.NEXT_PUBLIC_STACK_PROJECT_ID,
   publishableClientKey: env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
   secretServerKey: env.STACK_SECRET_SERVER_KEY,
   tokenStore: "cookie",
-}) as StackServerAppJsType;
+});
