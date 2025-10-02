bca52109daa918955ae434a83f15c1b73440fdd5
austinpower1258 <austinwang115@gmail.com>
2025-10-02 15:27:59 -0700

    new morph image!

--- apps/www/lib/utils/morph-defaults.ts
+++ apps/www/lib/utils/morph-defaults.ts
@@ -2,4 +2,4 @@
 // export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_osr8ngj4";
 // export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_r6j3rkmo";
 // export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_xo8ptbkl";
-export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_otir6x62";
+export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_x4zc18s4";
