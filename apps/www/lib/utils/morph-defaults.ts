c96478e184860e8bd0d941b418ace46d3b2f6510
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-10-06 13:43:39 -0700

    wip

--- apps/www/lib/utils/morph-defaults.ts
+++ apps/www/lib/utils/morph-defaults.ts
@@ -3,4 +3,5 @@
 // export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_r6j3rkmo";
 // export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_xo8ptbkl";
 // export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_x4zc18s4";
-export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_3w6tkrw5";
+// export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_3w6tkrw5";
+export const DEFAULT_MORPH_SNAPSHOT_ID = "snapshot_qtid11oa";
