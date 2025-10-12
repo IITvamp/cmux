a26ebb9afd1d1bedd345f1a8ce755a00d1c2cc01
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-10-12 00:47:13 +0000

    chore(opencode-grok-code): for the "Add updated instructions or context" input, we n...

--- apps/client/src/routes/_layout.$teamSlugOrId.task.$taskId.run.$runId.diff.tsx
+++ apps/client/src/routes/_layout.$teamSlugOrId.task.$taskId.run.$runId.diff.tsx
@@ -291,7 +291,7 @@ const RestartTaskForm = memo(function RestartTaskForm({
             branch={task?.baseBranch ?? undefined}
             environmentId={task?.environmentId ?? undefined}
             persistenceKey={persistenceKey}
-            maxHeight="42px"
+             maxHeight="300px"
             minHeight="30px"
             onEditorReady={(api) => {
               editorApiRef.current = api;
